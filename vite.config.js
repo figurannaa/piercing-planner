96c099a1b800b620243905e1a7f615551be518a1
k4t4 <kedvesk4t4@gmail.com>
2025-03-05 19:29:18 +0100

    Tiny fix for vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-/** 
-*   @type {import('vite').UserConfig}
+/**
+* @type {import('vite').UserConfig}
 */
 export default {
     base: process.env.NODE_ENV === 'production' ? '/piercing-planner/' : ''
-}
+  }
